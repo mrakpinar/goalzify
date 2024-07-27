68165981c29160807434ab9183dffa97876b6089
Murat Rahmi Akpınar <68166862+mrakpinar@users.noreply.github.com>
2024-07-28 00:07:24 +0300

    Update firebase_config.js

--- web/firebase_config.js
+++ web/firebase_config.js
@@ -1,11 +1,11 @@
 export function initializeApp() {
     const firebaseConfig = {
-        apiKey: "AIzaSyAqtlhVI5N_JUNIwdg26rhvZBPnO2Ku70Q",
-        authDomain: "goalzify-aa5a6.firebaseapp.com",
-        projectId: "goalzify-aa5a6",
-        storageBucket: "goalzify-aa5a6.appspot.com",
-        messagingSenderId: "1059881546953",
-        appId: "1:1059881546953:web:b96f732d27e8cd585c4918"
+        apiKey: "API_KEY",
+        authDomain: "AUTH_DOMAIN",
+        projectId: "PROJECT_ID",
+        storageBucket: "STORAGE_BUCKET",
+        messagingSenderId: "MESSAGING_SENDER_ID",
+        appId: "APP_ID"
     };
     firebase.initializeApp(firebaseConfig);
 }
